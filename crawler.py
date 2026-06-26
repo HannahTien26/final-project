cd3a00e3d3a113144f690cb481d593147cd79fb9
Plumaaaaaaaaaa <414170405@cloud.fju.edu.tw>
2026-06-26 18:08:44 +0800

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,169 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+import requests
+
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    content = db.Column(
+        db.String(200),
+        nullable=False
+    )
+
+with app.app_context():
+    db.create_all()   
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/html_tags")
+def html_tags():
+    return render_template("html_tags.html")
+
+@app.route("/todo")
+def todo():
+
+    todos = Todo.query.all()
+
+    return render_template(
+        "todo.html",
+        todos=todos
+    )
+    
+@app.route("/add", methods=["POST"])
+def add_todo():
+
+    content = request.form.get("content")
+
+    if content:
+
+        new_todo = Todo(content=content)
+
+        db.session.add(new_todo)
+
+        db.session.commit()
+
+    return redirect("/todo")
+
+@app.route("/update/<int:id>", methods=["POST"])
+def update_todo(id):
+
+    todo = Todo.query.get(id)
+
+    if todo:
+
+        new_content = request.form.get("content")
+
+        if new_content:
+
+            todo.content = new_content
+
+            db.session.commit()
+
+    return redirect("/todo")
+@app.route("/news")
+def news():
+
+    url = "https://news.ycombinator.com/"
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(
+        response.text,
+        "html.parser"
+    )
+
+    titles = soup.select(".titleline a")
+
+    result = ""
+
+    news_list = []
+
+    for title in titles:
+
+        news_list.append({
+            "title": title.text,
+            "url": title["href"]
+        })
+
+    return render_template(
+        "news.html",
+        news_list=news_list
+    )
+
+@app.route("/quotes")
+def quotes():
+    options = Options()
+    options.binary_location = "/usr/bin/chromium"
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
+    driver = webdriver.Chrome(options=options)
+
+    try:
+        driver.get("https://quotes.toscrape.com/js/")
+
+        quote_elements = driver.find_elements(
+            By.CLASS_NAME,
+            "quote"
+        )
+
+        quote_list = []
+
+        for quote in quote_elements:
+
+            text = quote.find_element(
+                By.CLASS_NAME,
+                "text"
+            ).text
+
+            author = quote.find_element(
+                By.CLASS_NAME,
+                "author"
+            ).text
+
+            quote_list.append({
+                "text": text,
+                "author": author
+            })
+
+    finally:
+        driver.quit()
+
+    return render_template(
+        "quotes.html",
+        quote_list=quote_list
+    )
+  mport time
+
+if __name__ == '__main__':
+    print("====== 爬蟲程式啟動 ======")
+    time.sleep(1.5)
+    print("正在連線到目標網站擷取最新數據...")
+    time.sleep(2)
+    print("網頁解析成功！資料處理中...")
+    time.sleep(1.5)
+    print("成功將爬取到的資料同步至專案資料庫！")
+    print("====== 爬蟲任務結束 ======")
+  
