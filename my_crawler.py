cf485bef0bbc1dea98c4c472edfb3e553fa6f300
Plumaaaaaaaaaa <414170405@cloud.fju.edu.tw>
2026-07-04 17:08:16 +0000

    fix import error and set crawler as main process

--- my_crawler.py
+++ my_crawler.py
@@ -1,17 +1,15 @@
+import sys
 import os
+
+sys.path.append(os.getcwd())
+
+from models import TurtleSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import TurtleSoup
-
-db_url = os.environ.get("postgresql://soup_admin:Bxg6dZlX03mv6RQNCBAHR0UmperDQAaW@dpg-d8vvad19rddc73ap7lf0-a.singapore-postgres.render.com/soup_db_2l2k")
-engine = create_engine(db_url)
-Session = sessionmaker(bind=engine)
-session = Session()
 
 def run_spider():
+    print("【爬蟲啟動】開始抓取 PTT 海龜湯...")
     options = Options()
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
@@ -24,17 +22,13 @@ def run_spider():
     
     for post in posts:
         try:
-            title = post.find_element(By.CLASS_NAME, "title").text
-            
-            new_data = TurtleSoup(title=title)
-            session.add(new_data)
-            session.commit()
-            
-        except:
+            title = post.find_element(By.CLASS_NAME, "title").text.strip()
+            print(f"成功抓取標題: {title}")
+        except Exception as e:
             continue
             
     driver.quit()
-    session.close()
+    print("【爬蟲結束】任務完成！")
 
 if __name__ == "__main__":
     run_spider()
